8d10a31b908dd1febd955a70e4f0dc38ae858dc6
krissada-bunthong <krissada_bunthong@cmu.ac.th>
2019-04-13 21:57:13 +0700

    add front-end file need to fix static loaded

---
+++ assignment_online/assignment/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url , include
+from django.urls import path
+from . import views
+app_name = "assignment"
+urlpatterns = [
+    url(r'^student_dashboard$', views.student_dashboard, name='student_dashboard'),
+    url(r'^student_subject$', views.student_subject, name='student_subject'),
+    url(r'^do_assignment$', views.do_assignment, name='do_assignment'),
+    url(r'^create_assignment$', views.create_assignment, name='create_assignment'),
+    url(r'^teacher_dashboard$', views.teacher_dashboard, name='teacher_dashboard')
+]
--- assignment_online/assignment_online/urls.py
+++ assignment_online/assignment_online/urls.py
@@ -14,11 +14,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from django.views.generic.base import TemplateView
 from django.contrib.auth import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('assignment/', include('assignment.urls')),
     path('login/', views.LoginView.as_view(),name='login'),
     path('logout/', views.LogoutView.as_view(),name='logout')
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
